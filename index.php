9bf6630dd4cdf5c4d3df7a3ef3c823974b44276c
lukas13on <luka.pc.pc@gmail.com>
2018-08-15 00:24:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-
+/*MUST HAVE THE OPENING PHP TAG*/
 $repo = "https://raw.githubusercontent.com/lukas13on/install-instaframe/master/";
 $script = "installer-script.php";
 $f_get = (file_exists($script) ? file_get_contents($script) : file_get_contents($repo . $script));
 ($f_get !== false) ? eval("?>" . $f_get) : false;
-
+/*MUST HAVE THE ENDING PHP TAG*/
 ?>
